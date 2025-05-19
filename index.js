b87e3978aa5a76158df32d12546b0281c0255b8f
Maruf Rahman  Mahin <30603593+Mahin4897@users.noreply.github.com>
2025-05-20 02:55:11 +0600

    Basic typescript learning

---
+++ index.js
@@ -0,0 +1,19 @@
+var myname = "mahin"; //~ a string type variable
+var age = 20; //~ a number type variable
+var isMale = true; //~ a boolean type variable
+var ages = [10, 20, 30, 40, 50]; //~ array declaration for typescript
+// ~ This is a custom type that definese a person object 
+// ~ and specifies the type of each property
+var person1 = {
+    name: "mahin",
+    age: 20,
+    isMale: true,
+    adress: {
+        city: "dhaka",
+        street: "mirpur",
+        country: "bangladesh"
+    }
+};
+//~ example of person type
+var group = [];
+console.log(person1);
--- index.ts
+++ index.ts
@@ -1,3 +1,53 @@
-let myname :string ="mahin"; //a string type variable
-let age :number = 20; //a number type variable
-let isMale :boolean = true; //a boolean type variable
+let myname :string ="mahin"; //~ a string type variable
+let age :number = 20; //~ a number type variable
+let isMale :boolean = true; //~ a boolean type variable
+let ages: number[]=[10,20,30,40,50] //~ array declaration for typescript
+
+//~ string number and boolean are primitive types
+//~ custom types
+type Address={
+    city:string
+    street:string
+    country:string
+}
+type Person={
+    name:string
+    age:number
+    isMale :boolean
+    adress?:Address
+    // ? means optional property
+}
+// ~ This is a custom type that definese a person object 
+// ~ and specifies the type of each
+
+
+type Updateperson=Partial<Person>
+// ~ This is a custom type that defines a partial person object
+// ~ and specifies the type of each property as optional.
+
+let person1 :Person ={
+    name:"mahin",
+    age:20,
+    isMale:true,
+    adress:{
+        city:"dhaka",
+        street:"mirpur",
+        country:"bangladesh"
+            }
+}
+//~ example of person type
+
+
+let group:Person[]=[]
+//~ array of person type
+
+type Userrole="admin"|"user"|"guest"
+let user1:Userrole="admin"
+//~  a custom type that defines a set of possible values
+//~ here user1 can only  be admin or user or 
+
+
+function getuser<T>(values:T):T{
+    return values
+}
+//~ this is a generic function that takes a value of any type and returns the same type
